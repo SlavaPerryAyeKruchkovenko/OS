d9172b85605826fbd31e6297cbc8bafe23f733a2
Slava <sh-e-95@mail.ru>
2022-05-03 02:05:23 +0500

    create complex lib

--- desktop/lab2/Test/main.cpp
+++ desktop/lab2/Test/main.cpp
@@ -12,11 +12,18 @@ double Sum(double num1, double num2);
 
 double Sub(double num1, double num2);
 
+void Mul1(double x1,double y1,double x2,double y2);
+void Sub1(double x1,double y1,double x2,double y2);
+void Sum1(double x1,double y1,double x2,double y2);
+
 #ifdef __cplusplus
 
 }
 #endif
+using namespace std;
+
 int main() {
     std::cout << Sum(2,2) << std::endl;
+    Sum1(2,2,6,5);
     return 0;
 }
--- desktop/lab2/complex2/library.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMPLEX2_LIBRARY_H
-#define COMPLEX2_LIBRARY_H
-
-void hello();
-
-#endif //COMPLEX2_LIBRARY_H
