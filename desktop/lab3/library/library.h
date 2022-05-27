6bce79f64af9f322a5b130c87fb2acc19dc192eb
Slava <sh-e-95@mail.ru>
2022-05-27 14:15:30 +0500

    create library

---
+++ desktop/lab3/library/library.h
@@ -0,0 +1,8 @@
+#ifndef LIBRARY_LIBRARY_H
+#define LIBRARY_LIBRARY_H
+
+double Mul(double num1, double num2);
+double Sum(double num1, double num2);
+double Sub(double num1, double num2);
+
+#endif //LIBRARY_LIBRARY_H
