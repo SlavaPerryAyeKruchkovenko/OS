eb2f8b631d11421f7a84a1e93767394139aa922f
Slava <sh-e-95@mail.ru>
2022-05-31 13:37:40 +0500

    add lib

--- desktop/lab3/test/test/Program.cs
+++ desktop/lab3/test/test/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Reflection;
+
+Assembly SampleAssembly = Assembly.LoadFrom(pathAll);
+
+Console.WriteLine(SampleAssembly.GetName());
+foreach (Type oType in SampleAssembly.GetTypes())
+{
+    Console.WriteLine(oType.Name);
+
+    foreach(MemberInfo members in oType.GetMembers())
+    {
+        Console.WriteLine("    " + members.Name);
+    }
+    Console.WriteLine();
+}
